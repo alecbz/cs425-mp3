91c786e4267f18f9638b55956cc5caf9f4537246
Alec Benzer <alecbenzer@gmail.com>
2014-04-25 08:49:59 -0500

    basic distributed behavior should be working, no replicas yet

---
+++ kvstore.py
@@ -0,0 +1,39 @@
+import time
+from threading import Lock
+
+
+class KVStore(object):
+
+    'A simple thread-safe timestamped key-value store.'
+
+    def __init__(self):
+        self.data = {}
+        self.lock = Lock()
+
+    def get(self, key):
+        with self.lock:
+            if key in self.data:
+                return self.data[key]
+            else:
+                return None, None
+
+    def insert(self, key, value):
+        with self.lock:
+            if key in self.data:
+                return False
+            self.data[key] = (value, time.time())
+            return True
+
+    def update(self, key, value):
+        with self.lock:
+            if key not in self.data:
+                return False
+            self.data[key] = (value, time.time())
+            return True
+
+    def delete(self, key):
+        with self.lock:
+            if key not in self.data:
+                return False
+            del self.data[key]
+            return True
--- server.py
+++ server.py
@@ -3,14 +3,27 @@ with other servers."""
 import pickle
 import socket
 import time
-from collections import deque, namedtuple
-from threading import Thread, Lock, Condition
+from collections import namedtuple
+from threading import Thread, Lock
+
+from kvstore import KVStore
 
 GetRequest = namedtuple('GetRequest', 'key')
 GetResponse = namedtuple('GetResponse', 'key value timestamp')
 
+InsertRequest = namedtuple('InsertRequest', 'key value')
+InsertResponse = namedtuple('InsertResponse', 'key result')
+
+UpdateRequest = namedtuple('UpdateRequest', 'key value')
+UpdateResponse = namedtuple('UpdateResponse', 'key result')
+
+DeleteRequest = namedtuple('DeleteRequest', 'key')
+DeleteResponse = namedtuple('DeleteResponse', 'key result')
+
 
 class Server(object):
+
+    'A server for the key-value store'
 
     def __init__(self, addr, addrs):
         self.addr = addr
@@ -19,13 +32,12 @@ class Server(object):
 
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        self.data = {}
-        self.data_lock = Lock()
+        self.data = KVStore()
 
     def start(self):
-        t = Thread(target=self.run)
-        t.daemon = True
-        t.start()
+        thread = Thread(target=self.run)
+        thread.daemon = True
+        thread.start()
 
     def send_message(self, msg, peer):
         'Send `msg` to `peer` and wait for a response'
@@ -36,24 +48,27 @@ class Server(object):
         return self.receive(sock)
 
     def run(self):
+        '''Listening for incoming connections and process them in a
+        seperate thread.'''
         self.sock.bind(self.addr)
         self.sock.listen(5)
 
         while 1:
             conn, addr = self.sock.accept()
-            t = Thread(target=self.handle_connection, args=(conn, addr))
-            t.daemon = True
-            t.start()
+            handler = Thread(target=self.handle_connection, args=(conn, addr))
+            handler.daemon = True
+            handler.start()
 
     def hash_key(self, key):
         return hash(key) % len(self.addrs)
 
-    def send(self, obj, sock):
+    @classmethod
+    def send(cls, obj, sock):
         'Send the object `obj` over the socket `sock`'
         sock.send(pickle.dumps(obj))
 
-    
-    def receive(self, sock):
+    @classmethod
+    def receive(cls, sock):
         'Receive a pickle-serialized object from the socket `sock`'
         data = ''
         while 1:
@@ -64,55 +79,65 @@ class Server(object):
                 pass  # read more data
 
     def handle_connection(self, conn, addr):
-        print "new connection from {}".format(addr)
         while 1:
             msg = self.receive(conn)
             if isinstance(msg, GetRequest):
-                key = msg.key
-                with self.data_lock:
-                    if key not in self.data:
-                        msg = GetResponse(key, value=None, timestamp=None)
-                    else:
-                        value, timestamp = self.data[key]
-                        msg = GetResponse(key, value, timestamp)
-                self.send(msg, conn)
+                value, timestamp = self.data.get(msg.key)
+                self.send(GetResponse(msg.key, value, timestamp), conn)
+            elif isinstance(msg, InsertRequest):
+                result = self.data.insert(msg.key, msg.value)
+                self.send(InsertResponse(msg.key, result), conn)
+            elif isinstance(msg, UpdateRequest):
+                result = self.data.update(msg.key, msg.value)
+                self.send(UpdateResponse(msg.key, result), conn)
+            elif isinstance(msg, DeleteRequest):
+                result = self.data.delete(msg.key)
+                self.send(DeleteResponse(msg.key, result), conn)
+            else:
+                print "Unknown message type: {}".format(msg)
 
     def get(self, key, level):
         iden = self.hash_key(key)
         handler = self.addrs[iden]
         if handler == self.addr:
-            # I'm responsible for this key
-            print "I'm responsible for this key"
-            with self.data_lock:
-                try:
-                    value, timestamp = self.data[key]
-                    return value
-                except KeyError:
-                    return None
+            value, timestamp = self.data.get(key)
+            return value
         else:
-            print "I'm NOT responsible for this key"
+            # I'm not responsible for this key
             resp = self.send_message(GetRequest(key), handler)
             assert isinstance(resp, GetResponse)
             assert resp.key == key
             return resp.value
 
     def insert(self, key, value, level):
-        with self.data_lock:
-            if key in self.data:
-                return False
-            self.data[key] = (value, time.time())
-            return True
+        iden = self.hash_key(key)
+        handler = self.addrs[iden]
+        if handler == self.addr:
+            return self.data.insert(key, value)
+        else:
+            resp = self.send_message(InsertRequest(key, value), handler)
+            assert isinstance(resp, InsertResponse)
+            assert resp.key == key
+            return resp.result
 
     def update(self, key, value, level):
-        with self.data_lock:
-            if key not in self.data:
-                return False
-            self.data[key] = (value, time.time())
-            return True
+        iden = self.hash_key(key)
+        handler = self.addrs[iden]
+        if handler == self.addr:
+            return self.data.update(key, value)
+        else:
+            resp = self.send_message(UpdateRequest(key, value), handler)
+            assert isinstance(resp, UpdateResponse)
+            assert resp.key == key
+            return resp.result
 
     def delete(self, key):
-        with self.data_lock:
-            if key not in self.data:
-                return False
-            del self.data[key]
-            return True
+        iden = self.hash_key(key)
+        handler = self.addrs[iden]
+        if handler == self.addr:
+            return self.data.delete(key)
+        else:
+            resp = self.send_message(DeleteRequest(key), handler)
+            assert isinstance(resp, DeleteResponse)
+            assert resp.key == key
+            return resp.result
